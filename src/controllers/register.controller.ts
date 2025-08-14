36d7547e2f662768260e6ce9975481c7c151ae64
s-jin721 <lsj9134721@gmail.com>
2025-08-14 23:54:23 +0900

    asynchandler 미들웨어 작성

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -3,10 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 import { registerUser } from '../services/register.service';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await registerUser(req.body.id, req.body.name, req.body.password);
-        res.status(201).json({ message: '회원가입 성공' });
-    } catch (error) {
-        next(error); // 여기서 미들웨어로 넘김
-    }
+    await registerUser(req.body.id, req.body.name, req.body.password);
+    res.status(201).json({ message: '회원가입 성공' });
 };
--- src/routes/register.routes.ts
+++ src/routes/register.routes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { register } from '../controllers/register.controller';
 import { validateRegister } from '../middlewares/register.middleware';
+import { asyncHandler } from '../middlewares/async.handler';
 
 const router = express.Router();
 
 
 // POST /api/auth/register  — 회원가입 라우트
-router.post('/register', validateRegister, register);
+router.post('/register', validateRegister, asyncHandler(register));
 console.log('라우터에서 register 진입'); // 이게 뜨는지 확인
 
 
