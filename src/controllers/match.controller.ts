9b5119af15b60e659d3f95e9c47ed670253df51f
s-jin721 <lsj9134721@gmail.com>
2025-09-05 18:05:16 +0900

    매칭 조회하기, 매칭 신청하기 완료

--- src/controllers/match.controller.ts
+++ src/controllers/match.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
-import { createMatchRequest } from "../services/match.service";
-import { ValidationError } from "../utils/errors";
+import { createMatchRequest, applyMatchRequest } from "../services/match.service";
+import { ValidationError, NotFoundError } from "../utils/errors";
+import { Match } from "../models/match.model";
+
 
 export const create = async (req: Request, res: Response) => {
     const { teamId, date, location, players } = req.body;
@@ -19,6 +21,48 @@ export const create = async (req: Request, res: Response) => {
     return res.status(201).json({
         status: 201,
         message: "매칭 요청 생성 성공",
+        data: match,
+    });
+};
+
+export const list = async (req: Request, res: Response) => {
+    try {
+        const matches = await Match.find()
+            .sort({ createdAt: -1 }) // 최신순
+            .populate({
+                path: "team",
+                select: "teamName", // 팀 이름만 가져옴
+            })
+            .populate({
+                path: "leader",
+                select: "username", // 팀장 아이디(유저네임)
+            });
+
+        return res.status(200).json({
+            status: 200,
+            message: "매칭 목록 조회 성공",
+            data: matches,
+        });
+    } catch (error) {
+        throw new NotFoundError("조회할 매칭이 없습니다.");
+    }
+};
+
+export const apply = async (req: Request, res: Response) => {
+    const { teamId, matchId } = req.body;
+    if (!teamId || !matchId) {
+        throw new ValidationError("필수 값이 누락되었습니다.");
+    }
+
+    const match = await applyMatchRequest(
+        teamId,
+        (req as any).userId,
+        matchId
+    );
+
+    return res.status(201).json({
+        status: 201,
+        message: "매칭 신청 성공",
         data: match,
     });
 };
--- src/routes/match.routes.ts
+++ src/routes/match.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { asyncHandler, requireAuth } from "../middlewares/auth";
-import { create } from "../controllers/match.controller";
+import { create, list, apply } from "../controllers/match.controller";
 
 const router = Router();
 
@@ -8,5 +8,9 @@ router.use(requireAuth);
 
 // 매칭 요청 생성 (팀장만 가능)
 router.post("/", asyncHandler(create));
+
+router.get("/list", asyncHandler(list));
+
+router.post("/apply", asyncHandler(apply));
 
 export default router;
