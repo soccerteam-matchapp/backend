9f2ba8b5ba99a392e74c4c93b1a5a3d6bb6a32ca
s-jin721 <lsj9134721@gmail.com>
2025-09-28 06:55:42 +0900

    팀원 참가 여부 투표기능 만들기

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { errorHandler } from './middlewares/error.handler';
 
 import teamRoutes from './routes/team.routes';
 import matchRoutes from './routes/match.routes';
+import attendancePoll from './routes/attendancePoll.routes';
 import phoneRoutes from './routes/phone.routes';
 
 import path from 'path';
@@ -31,6 +32,7 @@ app.use(errorHandler);
 
 app.use('/api/teams', teamRoutes);   // ← 인증 미들웨어는 라우터 안에서 적용됨
 app.use('/api/matches', matchRoutes);   // ← 인증 미들웨어는 라우터 안에서 적용됨
+app.use('/api/attendancePoll', attendancePoll);   // ← 인증 미들웨어는 라우터 안에서 적용됨
 app.use('/api/auth/phone', phoneRoutes);
 
 const PORT = Number(process.env.PORT || 3000);
