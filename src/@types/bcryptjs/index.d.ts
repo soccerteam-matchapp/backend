caadf8ba01a26d8171318edc7858235c54f01ff1
hyewon <j65995617@gmail.com>
2025-10-03 01:27:52 +0900

    fix: 파일 삭제

--- src/@types/bcryptjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "bcryptjs" {
-    // 최소 선언: 런타임만 통과하면 되므로 any로 둠
-    export function genSaltSync(rounds?: number): string;
-    export function hashSync(data: string, salt: string): string;
-    export function compareSync(data: string, encrypted: string): boolean;
-    const _default: any;
-    export default _default;
-}
--- src/types/solapi.d.ts
+++
@@ -1,2 +0,0 @@
-// src/types/solapi.d.ts
-declare module 'solapi';
